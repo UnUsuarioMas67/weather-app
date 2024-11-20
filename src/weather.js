bd3fe29b03c34e8e2b2adf671bb1afdb9017c4b4
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2024-11-20 00:04:13 -0400

    Add weather.js

---
+++ src/weather.js
@@ -0,0 +1,98 @@
+import {
+  fromUnixTime as dateFromUnixTime,
+  format as formatDate,
+} from "date-fns";
+
+const API_KEY = "HPBLYE9ZNT2US2HH8RP5DFP8X";
+
+async function fetchWeatherData(location, unitGroup = "metric") {
+  try {
+    const response = await fetch(
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=${API_KEY}&unitGroup=${unitGroup}&elements=tempmax,tempmin,temp,conditions,description,datetime,datetimeEpoch,hours,icon&iconSet=icons2`,
+      { mode: "cors" },
+    );
+    const weather = await response.json();
+
+    return weather;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function getCurrentWeather(weather) {
+  const location = weather.resolvedAddress;
+  const date = dateFromUnixTime(weather.currentConditions.datetimeEpoch);
+  const formattedDate = formatDate(date, "EEEE',' MMM do");
+  const icon = weather.currentConditions.icon;
+  const temp = weather.currentConditions.temp;
+  const tempmin = weather.days[0].tempmin;
+  const tempmax = weather.days[0].tempmax;
+  const conditions = weather.currentConditions.conditions;
+
+  return {
+    location,
+    formattedDate,
+    icon,
+    temp,
+    tempmin,
+    tempmax,
+    conditions,
+  };
+}
+
+function get24HourWeather(weather) {
+  const output = [];
+
+  const currentHour = weather.currentConditions.datetime;
+  let dayIndex = 0;
+  let hoursLeft = 24;
+
+  while (hoursLeft > 0) {
+    const hours = weather.days[dayIndex].hours;
+    const nextHourIndex =
+      hours.findIndex((hour) => hour.datetime === currentHour) + 1;
+
+    const startingIndex = dayIndex === 0 ? nextHourIndex : 0;
+
+    for (let i = startingIndex; i < hours.length; i++) {
+      const time = hours[i].datetime.split(":").slice(0, 2).join(":");
+      const icon = hours[i].icon;
+      const temp = hours[i].temp;
+
+      output.push({ time, icon, temp });
+
+      hoursLeft--;
+      if (hoursLeft === 0) {
+        break;
+      }
+    }
+
+    dayIndex++;
+  }
+
+  return output;
+}
+
+function get6DaysForecast(weather) {
+  const output = [];
+
+  for (let i = 1; i < 7; i++) {
+    const dayObj = weather.days[i];
+    const date = dateFromUnixTime(dayObj.datetimeEpoch);
+    const formattedDate = formatDate(date, "E M'/'d");
+    const icon = dayObj.icon;
+    const tempmin = dayObj.tempmin;
+    const tempmax = dayObj.tempmax;
+
+    output.push({ formattedDate, icon, tempmin, tempmax });
+  }
+
+  return output;
+}
+
+export {
+  fetchWeatherData,
+  getCurrentWeather,
+  get24HourWeather,
+  get6DaysForecast,
+};
